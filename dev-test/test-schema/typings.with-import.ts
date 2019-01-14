bae9f87eeb09b84ca18ee1af9d5159ad8ae5a569
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-01-14 17:18:28 +0100

    Include graphql-import in examples

---
+++ dev-test/test-schema/typings.with-import.ts
@@ -0,0 +1,46 @@
+export type Maybe<T> = T | null;
+
+
+
+
+
+
+
+
+// ====================================================
+// Types
+// ====================================================
+
+
+
+export interface Query {
+
+  allUsers: (Maybe<User>)[];
+
+  userById?: Maybe<User>;
+
+  answer: number[];
+}
+
+
+export interface User {
+
+  id: number;
+
+  name: string;
+
+  email: string;
+}
+
+
+
+// ====================================================
+// Arguments
+// ====================================================
+
+export interface UserByIdQueryArgs {
+
+  id: number;
+}
+
+
